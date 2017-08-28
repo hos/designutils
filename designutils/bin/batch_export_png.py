51ad834a37dd94a907195493324adfdadedb989f
onursolmaz <onursolmaz@gmail.com>
2017-08-28 22:58:32 +0300

    First commit

---
+++ designutils/bin/batch_export_png.py
@@ -0,0 +1,32 @@
+import os
+import glob
+import sys
+import argparse
+import re
+import subprocess
+
+dir_option = "dir"
+ext_option = "ext"
+
+parser = argparse.ArgumentParser()
+parser.add_argument('files', metavar='N', type=str, nargs='+',
+                    help='Files for replacement')
+parser.add_argument("--dpi", type=str, help = "Output DPI")
+
+def __main__():
+    args = parser.parse_args()
+
+    for fpath in args.files:
+        pre, ext = os.path.splitext(fpath)
+        cmd = ["inkscape", fpath, "--export-png="+pre+".png"]
+
+        if args.dpi:
+            cmd += ["--export-dpi="+args.dpi]
+
+        print(">> "+" ".join(cmd))
+
+        subprocess.call(cmd)
+
+
+if __name__ == '__main__':
+    __main__()
---
+++ setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+from os import path
+from setuptools.extension import Extension
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name = "designutils",
+
+    version = "0.0",
+
+    description = "Design utils",
+
+    author = "Onur Solmaz",
+
+    author_email = "onursolmaz@gmail.com",
+
+    packages = find_packages(exclude=["contrib", "docs", "tests"]),
+
+    extras_require = {
+        "dev": ["check-manifest"],
+        "test": ["coverage"],
+    },
+
+    # package_data={
+    #     "sample": ["package_data.dat"],
+    # },
+
+    # data_files=[("my_data")],
+
+    install_requires = {
+        "numpy",
+        "scipy",
+    },
+
+    # ext_modules = ""),
+
+    entry_points = {
+        "console_scripts": [
+            "batch_export_png=designutils.bin.batch_export_png:__main__",
+        ],
+    },
+)
+
+
+
