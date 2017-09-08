53a82c648d7a263af3fdb4ef19943dcc2b54b032
onursolmaz <onursolmaz@gmail.com>
2017-09-08 11:02:11 +0300

    Minor

--- designutils/bin/batch_export_png.py
+++ designutils/bin/batch_export_png.py
@@ -12,13 +12,26 @@ parser = argparse.ArgumentParser()
 parser.add_argument('files', type=str, nargs='+',
                     help='Files for replacement')
 parser.add_argument("--dpi", type=str, help = "Output DPI")
+parser.add_argument("-o", "--output", type=str, help = "Output directory")
 
 def __main__():
     args = parser.parse_args()
 
+    if args.output:
+        if not os.path.exists(args.output):
+            os.makedirs(args.output)
+
+
     for fpath in args.files:
         pre, ext = os.path.splitext(fpath)
-        cmd = ["inkscape", fpath, "--export-png="+pre+".png"]
+        filename = pre+".png"
+
+        if args.output:
+            opath = os.path.join(args.output, filename)
+        else:
+            opath = filename
+
+        cmd = ["inkscape", fpath, "--export-png="+opath]
 
         if args.dpi:
             cmd += ["--export-dpi="+args.dpi]
